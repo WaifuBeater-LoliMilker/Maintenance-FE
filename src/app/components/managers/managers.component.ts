1af7b1a1241e4cf057f4e7c2ebbff4446586caf6
Waifu Beater <tech20@rtc.edu.vn>
2025-05-15 10:00:52 +0700

    add base for sidenav + navbar

--- src/app/components/managers/managers.component.ts
+++ src/app/components/managers/managers.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-managers',
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+  ],
   templateUrl: './managers.component.html',
   styleUrl: './managers.component.css',
 })
-export class ManagersComponent {}
+export class ManagersComponent {
+  isSide = false;
+}
